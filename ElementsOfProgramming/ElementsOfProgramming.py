5b8c8636268f849a3f6fa0ea7c9cf1722019c95d
JTyzz <jtyzzer@gmail.com>
2020-01-25 18:23:47 -0800

    4.1 count_bits

--- ElementsOfProgramming/ElementsOfProgramming.py
+++ ElementsOfProgramming/ElementsOfProgramming.py
@@ -1,1 +1,12 @@
+#program to count bits in an integer. O(n) time complexity
+
+def count_bits(x: int):
+    num_bits = 0
+    while x:
+        num_bits += x & 1
+        x >>= 1
+    return num_bits
+
+print (count_bits(3442))
+
 
