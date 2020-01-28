a51ddf5d913f946b1f57d42a7e347259a6640d53
JTyzz <jtyzzer@gmail.com>
2020-01-28 11:58:45 -0800

    5.1 move all even ints to beginning of array

--- ElementsOfProgramming/ElementsOfProgramming.py
+++ ElementsOfProgramming/ElementsOfProgramming.py
@@ -19,9 +19,9 @@ def reverse_digits(x: int):
     return -string_as_num if x < 0 else string_as_num
 #why isn't negative int returning negative answers? (hint: make sure you have a print statement!!!!!)
 
-print (reverse_digits(-432))
+#print (reverse_digits(-432))
 
-#using modulo method to reduce time complexity to O(n)
+#not using string method
 def reverse_digits_nostring(x: int):
     result, x_remaining = 0, abs(x)
     while x_remaining:
@@ -29,7 +29,36 @@ def reverse_digits_nostring(x: int):
         x_remaining //= 10
     return -result if x < 0 else result
 
-print(reverse_digits_nostring(-24))
+
+#print(reverse_digits_nostring(-24))
+
+#check if number is a palendrome
+def is_num_palendrome(x: int):
+    result, x_remaining = 0, abs(x)
+    while x_remaining:
+        result = result * 10 + x_remaining % 10
+        x_remaining //= 10
+    return "true" if result == x else "false"
+
+print (is_num_palendrome(4114))
+print (is_num_palendrome(4115))
+
+
+
+#move all even integers to the beginning of array
+def even_odd_array(A):
+    array_low, array_high = 0, len(A)-1
+    while array_low < array_high:
+        if A[array_low] % 2 == 0:
+            array_low += 1
+        else:
+            A[array_low],A[array_high] = A[array_high], A[array_low]
+            array_high -= 1
+    print(A)
+
+
+even_odd_array([3,4,6,7,8,9])
+
 
 
 
